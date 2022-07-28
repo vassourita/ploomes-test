a24cc36bd18548213ad590d7b2288ef2db3a3097
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2022-07-28 12:49:31 -0300

    Updated schema examples

--- src/PloomesTest.WebApi/Swagger/ExampleSchemaFilter.cs
+++ src/PloomesTest.WebApi/Swagger/ExampleSchemaFilter.cs
@@ -2,7 +2,7 @@ using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 using PloomesTest.Core.Dtos;
-
+using PloomesTest.Core.Entities;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace PloomesTest.WebApi.Swagger
@@ -17,13 +17,33 @@ namespace PloomesTest.WebApi.Swagger
                 {
                     ["federalDocument"] = new OpenApiString("12345678000190"),
                     ["name"] = new OpenApiString("Ploomes"),
-                    ["email"] = new OpenApiString("example@domain.com"),
+                    ["email"] = new OpenApiString("example@ploomes.com"),
                     ["phone"] = new OpenApiString("11912234556"),
                     ["address"] = new OpenApiString("Rua Ferreira de Araujo, Pinheiros, 79"),
                     ["city"] = new OpenApiString("São Paulo"),
                     ["state"] = new OpenApiString("SP"),
                     ["zipCode"] = new OpenApiString("05428000"),
                     ["country"] = new OpenApiString("Brasil")
+                };
+            }
+
+            if (context.Type == typeof(Client))
+            {
+                schema.Example = new OpenApiObject
+                {
+                    ["id"] = new OpenApiString(Guid.NewGuid().ToString()),
+                    ["type"] = new OpenApiInteger(0),
+                    ["typeName"] = new OpenApiString("Company"),
+                    ["federalDocument"] = new OpenApiString("12345678000190"),
+                    ["name"] = new OpenApiString("Ploomes"),
+                    ["email"] = new OpenApiString("example@ploomes.com"),
+                    ["phone"] = new OpenApiString("11912234556"),
+                    ["address"] = new OpenApiString("Rua Ferreira de Araujo, Pinheiros, 79"),
+                    ["city"] = new OpenApiString("São Paulo"),
+                    ["state"] = new OpenApiString("SP"),
+                    ["zipCode"] = new OpenApiString("05428000"),
+                    ["country"] = new OpenApiString("Brasil"),
+
                 };
             }
         }
