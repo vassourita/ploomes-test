afbce9c37cadd323c18e73fb38efeab709e7e0eb
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2022-07-28 11:09:34 -0300

    Removed swagger ui from dev-only mode

--- src/PloomesTest.WebApi/Program.cs
+++ src/PloomesTest.WebApi/Program.cs
@@ -34,17 +34,14 @@ builder.Services.Configure<ApiBehaviorOptions>(
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    _ = app.UseSwagger()
-        .UseSwaggerUI(
-        options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-            options.RoutePrefix = string.Empty;
-        }
-    );
-}
+app.UseSwagger()
+    .UseSwaggerUI(
+    options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    }
+);
 
 app.UseHttpsRedirection();
 
